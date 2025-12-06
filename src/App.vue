dc47873f07792c8f6ccea1aae45da9dae787cde7
braddassow <brad@launchcodedevelopment.com>
2025-12-06 10:37:46 -0600

    Removed Data layer

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,12 @@
 
 import { Authenticator } from "@aws-amplify/ui-vue";
 import "@aws-amplify/ui-vue/styles.css";
-import Todos from './components/Todos.vue'
 </script>
 
 <template>
   <main>
     <authenticator>
       <template v-slot="{ signOut }">
-        <Todos />
         <button @click="signOut">Sign Out</button>
       </template>
     </authenticator>
